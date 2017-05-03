896ead3acd0cc8a1c918a43654d0b48e410e7f42
mriemer <mriemer@bloomhealthco.com>
2017-05-03 12:28:22 -0500

    Test for empty product name response

--- src/main/groovy/com/myRetail/client/ProductNameClient.groovy
+++ src/main/groovy/com/myRetail/client/ProductNameClient.groovy
@@ -9,6 +9,7 @@ import org.springframework.web.client.RestTemplate
  * Created by miker on 4/30/17.
  */
 class ProductNameClient {
+    private static String EXCLUDES = 'taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics'
     private RestTemplate restTemplate
     private String resourceUri
 
@@ -17,12 +18,13 @@ class ProductNameClient {
         this.resourceUri = resourceUri
     }
 
-    //TODO: handle 404 errors cleanly, throw 404 upstream
     public ProductName getProductName(Long id) {
-        ResponseEntity<RedskyEntity> responseEntity = restTemplate.getForEntity(resourceUri + "/v2/pdp/tcin/{id}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics", RedskyEntity.class, id)
-       ProductName productName = new ProductName().with {
+        ResponseEntity<RedskyEntity> responseEntity =
+                restTemplate.getForEntity(resourceUri + "/v2/pdp/tcin/{id}?excludes=" + EXCLUDES, RedskyEntity.class, id)
+
+        ProductName productName = new ProductName().with {
             productId = id
-            productName = responseEntity.body.product.item.productDescription.title
+            productName = responseEntity.body.product?.item?.productDescription?.title
             it
         }
 
